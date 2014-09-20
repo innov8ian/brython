94ffd9f8fcac2a2b29dcc22088dfb2ad74e85275
team-python <ian.team.python@gmail.com>
2014-09-20 21:27:01 +1000

    1st working one

---
+++ react.py
@@ -0,0 +1,10 @@
+from browser import document,alert,html
+doc=document
+def echo(ev):
+  alert(document["zone"].value)
+def saveTime(ev):
+  pass
+document['mybutton'].bind('click',echo)
+
+doc <= html.B("Hello world !")
+print("hi")
